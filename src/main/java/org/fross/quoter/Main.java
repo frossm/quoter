870b3214283071d6be0be6413ef76745ba2e2b14
Michael Fross <michael@fross.org>
2020-08-07 09:27:32 -0500

    Change header colors to be less dramatic

--- src/main/java/org/fross/quoter/Main.java
+++ src/main/java/org/fross/quoter/Main.java
@@ -128,7 +128,7 @@ public class Main {
 		// If symbols were entered, display the header for them
 		if (symbolList.size() > 0) {
 			Output.printColorln(Ansi.Color.CYAN, "-------------------------------------------------------------------------------");
-			Output.printColorln(Ansi.Color.YELLOW, "Symbol   Current    Chng   Chng%  DayHigh   Daylow  52WHigh   52WLow     YTD");
+			Output.printColorln(Ansi.Color.WHITE, "Symbol   Current    Chng   Chng%  DayHigh   Daylow  52WHigh   52WLow     YTD");
 			Output.printColorln(Ansi.Color.CYAN, "-------------------------------------------------------------------------------");
 		}
 
@@ -247,7 +247,7 @@ public class Main {
 
 		// Display Index Output Header
 		Output.printColorln(Ansi.Color.CYAN, "-------------------------------------------------------------------------------");
-		Output.printColorln(Ansi.Color.YELLOW, "Index        Current      Chng      Chng%");
+		Output.printColorln(Ansi.Color.WHITE, "Index        Current      Chng      Chng%");
 		Output.printColorln(Ansi.Color.CYAN, "-------------------------------------------------------------------------------");
 
 		// Loop through the three indexes and display the results
