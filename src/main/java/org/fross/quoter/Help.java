8c82af77fb5e20ab784b543158fa19ef319968a8
Michael Fross <michael@fross.org>
2021-07-06 14:14:49 -0500

    Tweaked help to simplfy

--- src/main/java/org/fross/quoter/Help.java
+++ src/main/java/org/fross/quoter/Help.java
@@ -54,13 +54,13 @@ public class Help {
 		Output.printColorln(Ansi.Color.WHITE, "Command Line Options");
 
 		Output.printColorln(Ansi.Color.YELLOW, "\nConfiguration:");
-		Output.printColorln(Ansi.Color.WHITE, "  -c        Configure the IEXCloud secret key. See GitHub at for details.");
+		Output.printColorln(Ansi.Color.WHITE, "  -b        Use IEXCloud Sandbox URL instead of production. Must be FIRST switch");
+		Output.printColorln(Ansi.Color.WHITE, "  -c        Configure the IEXCloud secret key. See GitHub for details.");
 		Output.printColorln(Ansi.Color.WHITE, "  -k        Display the IEXCloud secret key being used");
 		Output.printColorln(Ansi.Color.WHITE, "  -s        Save securities as default and show them automatically");
 		Output.printColorln(Ansi.Color.WHITE, "  -r        Remove saved securities");
-		Output.printColorln(Ansi.Color.WHITE, "  -i        Ignore saved queries for this execution only");
+		Output.printColorln(Ansi.Color.WHITE, "  -i        Ignore saved queries for this execution");
 		Output.printColorln(Ansi.Color.WHITE, "  -z        Disable colorized output");
-		Output.printColorln(Ansi.Color.WHITE, "  -b        Use IEXCloud Sandbox URL instead of production");
 		Output.printColorln(Ansi.Color.WHITE, "  -w WIDTH  Width, in columns, of output for the trending display");
 		Output.printColorln(Ansi.Color.WHITE, "  -n        Hide the Index display and just show stock quotes");
 		
@@ -71,7 +71,7 @@ public class Help {
 
 		Output.printColorln(Ansi.Color.YELLOW, "\nMisc:");
 		Output.printColorln(Ansi.Color.WHITE, "  -I        Display IEXCloud credit information for month");
-		Output.printColorln(Ansi.Color.WHITE, "  -D        Start in debug mode and display developer information");
+		Output.printColorln(Ansi.Color.WHITE, "  -D        Start in debug mode and display details for developers");
 		Output.printColorln(Ansi.Color.WHITE, "  -v        Display program version and lastest GitHub release and exit");
 		Output.printColorln(Ansi.Color.WHITE, "  -? | -h   Display this help information");
 
